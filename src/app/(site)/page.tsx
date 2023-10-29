cec01c67be1a2b282fc6928a81cc59e415a3a1bf
ratiomark <yanagae@gmail.com>
2023-10-30 00:08:36 +0200

    ListItem add href, image, animations

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -8,7 +8,11 @@ export default function Home() {
 				<div className='mb-2'>
 					<h1 className='text-3xl font-semibold text-white'>Welcome back</h1>
 					<div className='mt-4 grid grid-cols-1 gap-3 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
-						<ListItem />
+						<ListItem
+							image='/images/liked.png'
+							name='Liked Songs'
+							href='liked'
+						/>
 					</div>
 				</div>
 			</Header>
--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,11 +1,35 @@
 'use client'
 
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { FaPlay } from 'react-icons/fa'
+
 interface ListItemProps {
 	image: string
 	name: string
 	href: string
 }
 const ListItem = ({ image, name, href }: ListItemProps) => {
-	return <div>ListItem</div>
+	const router = useRouter()
+	const onClick = () => {
+		//add auth before push
+		router.push(href)
+	}
+	return (
+		<button className='group relative flex items-center gap-x-4 overflow-hidden rounded-md bg-neutral-100/10 pr-4 transition hover:bg-neutral-100/20'>
+			<div className='relative min-h-[64px] min-w-[64px] '>
+				<Image
+					className='object-cover'
+					fill
+					src={image}
+					alt='image'
+				/>
+			</div>
+			<p className='truncate py-5 font-medium'>{name}</p>
+			<div className='absolute opacity-0 transition rounded-full flex  items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110'>
+				<FaPlay />
+			</div>
+		</button>
+	)
 }
 export default ListItem

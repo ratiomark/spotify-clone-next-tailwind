54a948e20c04d42ee0774125b07fdb38b1cf3b1b
ratiomark <yanagae@gmail.com>
2023-10-30 15:30:40 +0200

    add: song item

--- src/components/SongItem.tsx
+++ src/components/SongItem.tsx
@@ -2,6 +2,8 @@ import useLoadImage from '@/hooks/useLoadImage'
 import { Song } from '@/shared/types/types'
 import Image from 'next/image'
 
+import PlayButton from './PlayButton'
+
 interface SongItemProps {
 	data: Song
 	onClick: () => void
@@ -27,11 +29,32 @@ const SongItem = ({ data, onClick }: SongItemProps) => {
 			transition
 			'
 		>
-			<Image
-				fill
-				src={imagePath ?? ''}
-				alt='cover'
-			/>
+			<div
+				className='
+				relative
+				aspect-square
+				h-full
+				w-full
+				overflow-hidden
+				rounded-md
+				'
+			>
+				<Image
+					className='object-cover'
+					fill
+					src={imagePath ?? '/images/liked.png'}
+					alt='cover'
+				/>
+			</div>
+			<div className='flex w-full flex-col items-start gap-1 p-4'>
+				<p className='w-full truncate font-semibold'>{data.title}</p>
+				<p className='w-full truncate pb-4 text-sm text-neutral-400'>
+					{data.author}
+				</p>
+			</div>
+			<div className='absolute bottom-24 right-5'>
+				<PlayButton />
+			</div>
 		</div>
 	)
 }
